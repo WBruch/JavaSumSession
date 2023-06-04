e47bcd4eef1ab9faf841bc328dd1e6cb6f85a23a
student <valeriibruch@gmail.com>
2023-06-04 13:05:47 +0200

    reformat

--- src/main/java/tasksSumSession_1/SumSession1.java
+++ src/main/java/tasksSumSession_1/SumSession1.java
@@ -42,7 +42,7 @@ public class SumSession1 {
         System.out.println("max value is: " + maxValue + ", min value is: " + minValue);
     }
 
-// второй способ решения Task 3
+    // второй способ решения Task 3
     private static void getMinMax2(int[] array) {
         int minValue = array[0];
         int maxValue = array[0];
@@ -55,11 +55,11 @@ public class SumSession1 {
 // Task 4
 // (Сложность 3) Напишите программу на Java, которая находит количество отрицательных элементов в целочисленном массиве и выводит результат.
 
-    private static int getAmountOfNegativeNumbers(int[]array) {
+    private static int getAmountOfNegativeNumbers(int[] array) {
         int count = 0;
 
-        for (int i:
-             array) {
+        for (int i :
+                array) {
             if (i < 0)
                 count++;
         }
@@ -74,8 +74,7 @@ public class SumSession1 {
 
         char[] chars = str.toCharArray();
 
-        for (int i = 0; i < chars.length; i++)
-        {
+        for (int i = 0; i < chars.length; i++) {
             if (chars[i] == target) {
                 return i;
             }
@@ -89,7 +88,7 @@ public class SumSession1 {
 // (Сложность 5) Напишите программу на Java, которая сортирует целочисленный массив в порядке возрастания, используя алгоритм сортировки слиянием (merge sort).
 
 
-    private static int[] getSortedArray(int[]array) {
+    private static int[] getSortedArray(int[] array) {
 
         if (array.length <= 1)
             return array;
@@ -120,16 +119,53 @@ public class SumSession1 {
     }
 
 
+// Task 7
+// (Сложность 5) Напишите программу на Java, которая находит наиболее часто встречающийся элемент в целочисленном массиве и выводит его значение.
+
+    private static int getFrequentElement(int[] array) {
+
+        int[] frequency = new int[array.length];
+        int totalCounter = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            int counter = 0;
+            int temp = array[i];
+            for (int j = 0; j < array.length; j++) {
+                if (temp == array[j]) {
+                    counter++;
+                }
+            }
+            frequency[i] = counter;
+            if (counter > totalCounter)
+                totalCounter = counter;
+        }
+
+        int result = 0;
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] == totalCounter ) {
+                result = array[i];
+            }
+        }
+
+        System.out.println(Arrays.toString(frequency));
+
+        return result;
+    }
+
+
+//Task 8
+//(Сложность 5) Напишите программу на Java, которая объединяет два отсортированных целочисленных массива в один отсортированный массив, используя алгоритм слияния (merge).
 
 
 
 
     public static void main(String[] args) {
-        int arr[] = {-4 ,1, 65, -2, 8, 32, 0, 90, 2, 10, 399, -27,};
+        int arr[] = {-4, 1, 65, -2, 8, 32, 0, 90, 8, 2, 10, 399, -27, 8};
         String str = "Hello world , Java summary session, friday. Tel-ran Berlin, Deutschland";
 
 //        System.out.println("Task 4. Kоличество негативных чисел в массиве равно: " + getAmountOfNegativeNumbers(arr));
 //        System.out.println("Task 5. Индекс первого вхождения заданного элемента в строковом массиве равен: " + findFirstEntryIndex(str, 'D'));
-//        System.out.println("Массив полученный в результате сортировки слиянием (merge sort): " + Arrays.toString(getSortedArray(arr)));
+//        System.out.println("Task 6. Массив полученный в результате сортировки слиянием (merge sort): " + Arrays.toString(getSortedArray(arr)));
+//        System.out.println("Task 7. Hаиболее часто встречающийся элемент в целочисленном массиве: " + getFrequentElement(arr));
     }
 }
